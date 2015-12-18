71d5126c1b1e7e5821624c63d7d96d44948b7815
Tad <terry.donaghe@gmail.com>
2015-12-17 17:35:11 -0800

    Added support for Bootstrap

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,10 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
+  <div className="container">
+    <div className="col-md-6">
+      <h1>Hello, Bootstrap!</h1>
+    </div>
+  </div>,
   document.getElementById('app')
 );
