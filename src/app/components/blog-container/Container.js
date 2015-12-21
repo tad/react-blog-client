47988e98ccffad68adb4d7531a91610de6994eef
Tad <terry.donaghe@gmail.com>
2015-12-21 14:27:24 -0800

    Sidebar content now passed as state properties

--- src/app/components/blog-container/Container.js
+++ src/app/components/blog-container/Container.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import BlogHeader from './header/BlogHeader';
 import Sidebar from './sidebar/Sidebar';
 
+// TEMPORARY IMPORTS TO SIMULATE DYNAMIC CONTENT
+import sidebar1 from './Sidebar/Sidebar1';
+import sidebar2 from './Sidebar/Sidebar2';
+import sidebar3 from './Sidebar/Sidebar3';
+
 export default React.createClass({
+  getInitialState: function() {
+    return {
+      sidebars: [sidebar1, sidebar2, sidebar3]
+    };
+  },
   render: function() {
     return(
       <div className="container">
@@ -12,7 +22,7 @@ export default React.createClass({
           <div className="col-sm-8 blog-main">
             <h1>Hello, React World!</h1>
           </div>
-          <Sidebar />
+          <Sidebar sidebars={this.state.sidebars} />
         </div>
       </div>
     );
--- src/app/components/blog-container/sidebar/Sidebar.js
+++ src/app/components/blog-container/sidebar/Sidebar.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// TEMPORARY IMPORTS TO SIMULATE DYNAMIC CONTENT
-import sidebar1 from './Sidebar1';
-import sidebar2 from './Sidebar2';
-import sidebar3 from './Sidebar3';
-
-// This will eventually be create from stuff put in state.
-var sidebars = [sidebar1, sidebar2, sidebar3];
 
 export default React.createClass({
   render: function() {
     return(
       <div className="col-sm-3 col-sm-offset-1 blog-sidebar">
-        {sidebars}
+        {this.props.sidebars}
       </div>
     );
   }
