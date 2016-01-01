fc463a98344d958fb4d5e69b7ff745f374fd37b6
Tad <terry.donaghe@gmail.com>
2016-01-01 15:40:12 -0800

    Added react-redux and added store at app level

--- src/app/App.js
+++ src/app/App.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import Main from './components/Main';
 import Footer from './components/Footer';
 import {Route, Router} from 'react-router';
+import { Provider } from 'react-redux';
 import routes from './config/routes';
 
+import { createStore } from 'redux'
+import myBlog from '../reducer'
+
+let store = createStore(myBlog);
+
+
 ReactDOM.render(
-  <Router>
-    {routes}
-  </Router>,
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
