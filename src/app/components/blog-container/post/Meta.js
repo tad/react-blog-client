53f24efd8416264d807607217be87d5a8a5a7672
Tad <terry.donaghe@gmail.com>
2016-01-04 19:02:16 -0800

    Updated meta and title blog components to use markdown

--- src/app/components/blog-container/post/Meta.js
+++ src/app/components/blog-container/post/Meta.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import marked from 'marked';
 
 export default React.createClass({
   propTypes: {
-    //meta: React.PropTypes.object.isRequired,    
+    meta: React.PropTypes.string.isRequired,
+  },
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.meta.toString(), {sanitize: true});
+    return { __html: rawMarkup };
   },
   render: function() {
     return(
-      <p className="blog-post-meta">
-        {this.props.meta}
-      </p>
+      <p className="blog-post-meta" dangerouslySetInnerHTML={this.rawMarkup()} />
     );
   }
 });
--- src/app/components/blog-container/post/Title.js
+++ src/app/components/blog-container/post/Title.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import marked from 'marked';
 
 export default React.createClass({
   propTypes: {
-    //title: React.PropTypes.string.isRequired
+    title: React.PropTypes.string.isRequired
+  },
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.title.toString(), {sanitize: true});
+    return { __html: rawMarkup };
   },
   render: function() {
     return(
-      <h2 className="blog-post-title">
-        {this.props.title}
-      </h2>
+      <h2 className="blog-post-title" dangerouslySetInnerHTML={this.rawMarkup()} />        
     );
   }
 });
