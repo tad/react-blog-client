d5239617f33a4e5d1c7a431ec232df106cb4e6b9
Tad <terry.donaghe@gmail.com>
2016-01-11 18:25:11 -0800

    Split up masthead tests

--- test/components/masthead_spec.js
+++ test/components/masthead_spec.js
@@ -7,27 +7,33 @@ import {List} from 'immutable';
 
 const {renderIntoDocument, scryRenderedDOMComponentsWithTag, scryRenderedDOMComponentsWithClass, Simulate} = React.addons.TestUtils;
 
+const mastHeadItems = [
+  {
+    link: '#',
+    title: 'Home',
+    default: true
+  },
+  {
+    link: '#/categories',
+    title: 'Categories',
+    default: false
+  }];
+
+  const component = renderIntoDocument(<Masthead masthead={mastHeadItems} />);
+
 describe('Masthead', () => {
   it('renders two items in the masthead', () => {
-    const mastHeadItems = [
-      {
-        link: '#',
-        title: 'Home',
-        default: true
-      },
-      {
-        link: '#/categories',
-        title: 'Categories',
-        default: false
-      }];
-
-    const component = renderIntoDocument(<Masthead masthead={mastHeadItems} />);
     const mastHeadLinks = scryRenderedDOMComponentsWithTag(component, 'a');
-    const activeLink = scryRenderedDOMComponentsWithClass(component, 'blog-nav-item active');
-
     expect(mastHeadLinks.length).to.equal(2);
+  });
+  it('contains the expected text content', () => {
+    const mastHeadLinks = scryRenderedDOMComponentsWithTag(component, 'a');
     expect(mastHeadLinks[0].textContent).to.equal('Home');
     expect(mastHeadLinks[1].textContent).to.equal('Categories');
+  });
+
+  it('properly styles the active masthead link', () => {
+    const activeLink = scryRenderedDOMComponentsWithClass(component, 'blog-nav-item active');
     expect(activeLink.length).to.equal(1);
     expect(activeLink[0].textContent).to.equal('Home');
   });
